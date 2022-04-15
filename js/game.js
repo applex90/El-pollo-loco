38aa657b20511989bfc284c10574c1fd4237202d
applex90 <alex20bachmann@gmx.de>
2022-04-15 14:57:37 +0200

    added chicken

--- js/game.js
+++ js/game.js
@@ -1,16 +1,11 @@
 let canvas;
-let ctx;
-let character = new Image();
+let world;
 
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
+    
 
-    character.src = '../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png';
-
-    setTimeout(function () {
-        ctx.drawImage(character, 20, 20, 50, 150);
-    }, 2000);
-
+    console.log('My Character is', world.character);
 }
