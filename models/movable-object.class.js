26029796191d5e2050c48ee4d066e960465572b4
applex90 <alex20bachmann@gmx.de>
2022-04-20 21:12:25 +0200

    cleaned functions

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -11,6 +11,20 @@ class MovableObject {
     speedY = 0;
     acceleration = 2.5;
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = '3';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
 
     applyGravity() {
         setInterval(() => {
--- models/world.class.js
+++ models/world.class.js
@@ -27,14 +27,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.translate(this.camera_x,0);
+        this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
 
-        this.ctx.translate(-this.camera_x,0);
+        this.ctx.translate(-this.camera_x, 0);
 
 
         // Draw() wird immer wieder aufgerufen
@@ -55,10 +55,10 @@ class World {
     addToMap(mo) {
         if (mo.otherDirection) {
             this.flipImage(mo);
-
-
         }
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
+        mo.draw(this.ctx);
+        mo.drawFrame(this.ctx);
 
         if (mo.otherDirection) {
             this.flipImageBack(mo);
@@ -73,6 +73,7 @@ class World {
         mo.x = mo.x * -1;
     }
 
+    
     flipImageBack(mo) {
         mo.x = mo.x * -1;
         this.ctx.restore();
