e56fa71aaea2bcdca6f2953d2ba12e9f3613db9f
applex90 <alex20bachmann@gmx.de>
2022-04-30 04:40:22 +0200

    added eventlistener

--- js/game.js
+++ js/game.js
@@ -2,8 +2,111 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-function init(){
+
+function init() {
     console.info('Startscreen');
+    setEventListener();    
+}
+
+function setEventListener(){
+    defineElements();
+    defineEvents();
+    console.log(up_element);
+    console.log(left_element);
+    console.log(space_element);
+    console.log(right_element);
+}
+
+
+function defineElements(){
+    up_element = document.getElementById('up-btn');
+    space_element = document.getElementById('space-btn');
+    left_element = document.getElementById('left-btn');
+    right_element = document.getElementById('right-btn');
+}
+
+
+function defineEvents(){
+    defineUP();
+    defineSPACE();
+    defineLEFT();
+    defineRIGHT();
+}
+
+
+function defineUP(){
+    up_element.addEventListener('touchstart', () => {
+        keyboard.UP = true;
+    });
+
+    up_element.addEventListener('mousedown', () => {
+        keyboard.UP = true;
+    });
+    
+    up_element.addEventListener('touchend', () => {
+        keyboard.UP = false;
+    });
+    
+    up_element.addEventListener('mouseup', () => {
+        keyboard.UP = false;
+    });
+}
+
+
+function defineSPACE(){
+    space_element.addEventListener('touchstart', () => {
+        keyboard.SPACE = true;
+    });
+
+    space_element.addEventListener('mousedown', () => {
+        keyboard.SPACE = true;
+    });
+    
+    space_element.addEventListener('touchend', () => {
+        keyboard.SPACE = false;
+    });
+
+    space_element.addEventListener('mouseup', () => {
+        keyboard.SPACE = false;
+    });
+}
+
+
+function defineLEFT(){
+    left_element.addEventListener('touchstart', () => {
+        keyboard.LEFT = true;
+    });
+
+    left_element.addEventListener('mousedown', () => {
+        keyboard.LEFT = true;
+    });
+    
+    left_element.addEventListener('touchend', () => {
+        keyboard.LEFT = false;
+    });
+
+    left_element.addEventListener('mouseup', () => {
+        keyboard.LEFT = false;
+    });
+}
+
+
+function defineRIGHT(){
+    right_element.addEventListener('touchstart', () => {
+        keyboard.RIGHT = true;
+    });
+
+    right_element.addEventListener('mousedown', () => {
+        keyboard.RIGHT = true;
+    });
+    
+    right_element.addEventListener('touchend', () => {
+        keyboard.RIGHT = false;
+    });
+
+    right_element.addEventListener('mouseup', () => {
+        keyboard.RIGHT = false;
+    });
 }
 
 
@@ -16,7 +119,7 @@ function startGame() {
 }
 
 
-function removeStartElements(){
+function removeStartElements() {
     document.getElementById('canvas').style.backgroundImage = 'none';
     document.getElementById('start-btn').style.display = 'none';
     toggleControlBar();
@@ -42,9 +145,6 @@ window.addEventListener("keydown", (e) => {
     if (e.keyCode == 32) {
         keyboard.SPACE = true;
     }
-    if (e.keyCode == 40) {
-        keyboard.DOWN= true;
-    }
 });
 
 
@@ -61,7 +161,5 @@ window.addEventListener("keyup", (e) => {
     if (e.keyCode == 32) {
         keyboard.SPACE = false;
     }
-    if (e.keyCode == 40) {
-        keyboard.DOWN= false;
-    }
 });
+
